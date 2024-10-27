511dd401b3ad59f26b86c8778ab616f5347acdb9
Babycake2332 <skoog.alexander@gmail.com>
2024-10-27 21:58:28 +0100

    added text node to html node conversion

--- src/main.py
+++ src/main.py
@@ -1,13 +1,30 @@
-from textnode import TextNode
+from textnode import TextNode, TextType
+from htmlnode import LeafNode
+
+def text_node_to_html_node(text_node):
+    if not isinstance(text_node.text_type, TextType):
+        raise TypeError("Invalid text type")
+
+    if text_node.text_type == TextType.TEXT:
+        text_node = LeafNode(value=text_node.text)
+    elif text_node.text_type == TextType.BOLD:
+        text_node = LeafNode(tag="b", value=text_node.text)
+    elif text_node.text_type == TextType.ITALIC:
+        text_node = LeafNode(tag="i", value=text_node.text)
+    elif text_node.text_type == TextType.CODE:
+        text_node = LeafNode(tag="code", value=text_node.text)
+    elif text_node.text_type == TextType.IMAGE:
+        text_node = LeafNode(tag="img", value="", props={"src": text_node.url, "alt": text_node.text})
+
 
 def main():
-    object = TextNode('Hey there folks', 'normal')
-    other_object = TextNode('Hey there folks', 'normal')
+    node = TextNode(text="hello there", text_type=TextType.BOLD)
+    text_node_to_html_node(node)
 
-    print(object.__eq__(other_object))
-    print(object)
 
 
 
 if __name__ == '__main__':
     main()
+
+
